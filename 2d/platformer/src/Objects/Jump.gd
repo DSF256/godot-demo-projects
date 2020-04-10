f2671e9d8e39e182865f5c528242df1005d79097
NotHacker101 <Cobra1234+>
2020-04-09 20:34:42 -0400

    Update Jump.gd
    Changed the jump boost comments to make it more clear and nicer coding, Because im taking the class as a masters class.

--- 2d/platformer/src/Objects/Jump.gd
+++ 2d/platformer/src/Objects/Jump.gd
@@ -6,11 +6,11 @@ onready var animation_player = $AnimationPlayer
 
 onready var jumpHigher = 1
 
-# The Coins only detects collisions with the Player thanks to its collision mask.
-# This prevents other characters such as enemies from picking up coins.
+# The jump boost icon only detects collisions with the Player thanks to its collision mask.
+# This prevents other characters such as enemies from picking up The jump boost.
 
-# When the player collides with a coin, the coin plays its 'picked' animation.
-# The animation takes cares of making the coin disappear, but also deactivates its
+# When the player collides with a The jump boost, the The jump boost plays its 'picked' animation.
+# The animation takes cares of making the The jump boost disappear, but also deactivates its
 # collisions and frees it from memory, saving us from writing more complex code.
 # Click the AnimationPlayer node to see the animation timeline.
 func _on_body_entered(_body):
